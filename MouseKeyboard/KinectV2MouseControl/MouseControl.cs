a06f7644ee63e39d707df48de490a556f107ca7b
R SREEHARI <sreehari.r96@gmail.com>
2016-10-09 14:42:18 +0530

    Mouse Control

---
+++ MouseKeyboard/KinectV2MouseControl/MouseControl.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace KinectMouseKeyboard
+{
+    class MouseKeyBoardControl
+    {
+        public static void MouseLeftDown()
+        {
+            mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+        }
+        public static void MouseLeftUp()
+        {
+            mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+        }
+
+        public static void DoMouseClick()
+        {
+            mouse_event(MouseEventFlag.LeftDown | MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+        }
+
+        public static void KeyUp()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("{UP}");
+        }
+
+        public static void KeyDown()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+
+            //SetForegroundWindow(calculatorHandle);
+            SendKeys.Send("{DOWN}");
+        }
+
+        public static void KeyLeft()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.Send("{LEFT}");
+        }
+
+        public static void KeyRight()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.Send("{RIGHT}");
+        }
+
+        public static void KeyCtrlTab()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.Send("+{TAB}");
+        }
+
+        public static void KeyAltTab()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.Send("%{TAB}");
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int X, int Y);
+        [DllImport("user32.dll")]
+        static extern void mouse_event(MouseEventFlag flags, int dx, int dy, uint data, UIntPtr extraInfo);
+        
+        [Flags]
+        enum MouseEventFlag : uint
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            XDown = 0x0080,
+            XUp = 0x0100,
+            Wheel = 0x0800,
+            VirtualDesk = 0x4000,
+            Absolute = 0x8000
+        }
+
+        /// <summary>
+        /// Struct representing a point.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <see>See MSDN documentation for further information.</see>
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+
+            return lpPoint;
+        }
+
+    }
+}
