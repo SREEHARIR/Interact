3a9be5a7b2694e4ce995aa49a096b07848601b39
R SREEHARI <sreehari.r96@gmail.com>
2016-10-19 14:48:55 +0530

    KeyBoard Functionality

---
+++ KinectWindowsUI/KinectWindowsUI/KinectWindowsUI/Keyboard.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace KinectWindowsUI
+{
+    class Keyboard
+    {
+        public static void KeyUp()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("{UP}");
+        }
+
+        public static void KeyDown()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+
+            //SetForegroundWindow(calculatorHandle);
+            SendKeys.SendWait("{DOWN}");
+        }
+
+        public static void KeyLeft()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("{LEFT}");
+        }
+
+        public static void KeyRight()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("{RIGHT}");
+        }
+
+        public static void KeyCtrlTab()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("+{TAB}");
+        }
+
+        public static void KeyAltTab()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("%{TAB}");
+        }
+
+        public static void KeyESC()
+        {
+            // Send the enter key to the button, which raises the click 
+            // event for the button. This works because the tab stop of 
+            // the button is 0.
+            SendKeys.SendWait("{ESC}");
+        }
+
+    }
+}
